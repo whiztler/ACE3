d4c2b81e6e46ca20fbf700c713fb25ccd2007968
Giallustio <giallustio@hotmail.com>
2015-01-14 11:20:20 +0100

    ace magazines
    ace magazines porting

---
+++ addons/magazines/config.cpp
@@ -0,0 +1,12 @@
+#include "script_component.hpp"
+
+class CfgPatches {
+	class ADDON {
+		units[] = {};
+		requiredVersion = REQUIRED_VERSION;
+		requiredAddons[] = { "A3_Weapons_F", "A3_Weapons_F_beta", "A3_Weapons_F_gamma", "ace_common" };
+		version = VERSION;
+	};
+};
+
+
---
+++ addons/magazines/script_component.hpp
@@ -0,0 +1,3 @@
+#define COMPONENT magazines
+#include "\z\ace\addons\main\script_mod.hpp"
+#include "\z\ace\addons\main\script_macros.hpp"
